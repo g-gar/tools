39c858a20e2feeb10294fb7e30ee479b252707c0
g-gar <g.gar.fern@hotmail.com>
2020-12-23 21:49:20 +0000

    Deleted files

--- argsparser/src/main/java/com/ggar/tools/argsparser/GeneratorMappings.java
+++
@@ -1,9 +0,0 @@
-package com.ggar.tools.argsparser;
-
-import com.ggar.tools.argsparser.condition.Condition;
-
-import java.util.HashMap;
-import java.util.function.Function;
-
-public class GeneratorMappings extends HashMap<Condition, Function> {
-}
